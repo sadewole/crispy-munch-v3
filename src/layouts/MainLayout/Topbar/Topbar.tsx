b5f42808f3e5010e083fd7a7e52b0c5fdf54c580
sadewole <samueladewole15@gmail.com>
2021-05-19 19:32:11 +0100

    user authenticated, topbar and popovers

--- src/layouts/MainLayout/Topbar/Topbar.tsx
+++ src/layouts/MainLayout/Topbar/Topbar.tsx
@@ -1,30 +1,63 @@
 import React from 'react';
 import { NavLink as RouterLink } from 'react-router-dom';
-import {
-  Link,
-  Flex, Spacer, Box, Button, Container
-} from "@chakra-ui/react";
+import { Link, Flex, Spacer, Box, Button, Container } from '@chakra-ui/react';
 import { Logo } from 'src/components/Logo';
+import { FaCartPlus } from 'react-icons/fa';
+import { useAuth } from 'src/context/authContext';
+import UserDropdown from './UserDropdown';
 
 type toggleFunc = () => void;
 
-const Topbar = ({ toggleLogin }: { toggleLogin: toggleFunc; }) => {
-
+const Topbar = ({ toggleLogin }: { toggleLogin: toggleFunc }) => {
+  const {
+    authState: { isAuthenticated, user },
+    logout,
+  } = useAuth();
   return (
-    <Box bg="transparent" py="5" overflow="hidden" marginBottom={{ base: "0", md: '10' }} >
-      <Container maxWidth="container.xl" >
+    <Box
+      bg='transparent'
+      py='5'
+      overflow='hidden'
+      marginBottom={{ base: '0', md: '10' }}
+    >
+      <Container maxWidth='container.xl'>
         <Flex>
-          <Box p="2">
-            <Link as={RouterLink} to="/" ><Logo /></Link>
+          <Box p='2'>
+            <Link as={RouterLink} to='/'>
+              <Logo />
+            </Link>
           </Box>
           <Spacer />
-          <Box p="2" >
-            <Link as={RouterLink} to="/menu" mr="4" >Menu</Link>
-            <Button bgColor="red.800" _hover={{ bg: "red.900" }} color="white" onClick={toggleLogin} >Login</Button>
+          <Box p='2' display='flex' alignItems='center'>
+            {isAuthenticated && <UserDropdown user={user} logout={logout} />}
+            <Link as={RouterLink} to='/menu' mr='4' color='red.800'>
+              Menu
+            </Link>
+            {!isAuthenticated ? (
+              <Button
+                bgColor='red.800'
+                _hover={{ bg: 'red.900' }}
+                color='white'
+                onClick={toggleLogin}
+              >
+                Login
+              </Button>
+            ) : (
+              <Button
+                leftIcon={<FaCartPlus />}
+                bgColor='red.800'
+                _hover={{ bg: 'red.900' }}
+                color='white'
+                as={RouterLink}
+                to='/cart'
+              >
+                Cart
+              </Button>
+            )}
           </Box>
         </Flex>
       </Container>
-    </Box >
+    </Box>
   );
 };
 
---
+++ src/layouts/MainLayout/Topbar/UserDropdown.tsx
@@ -0,0 +1,123 @@
+import React from 'react';
+import { createPopper } from '@popperjs/core';
+import { Box, ListItem, List, Text } from '@chakra-ui/layout';
+import { Link as RouterLink } from 'react-router-dom';
+import {
+  FiUserCheck,
+  FiChevronUp,
+  FiChevronDown,
+  FiLogOut,
+  FiInbox,
+  FiUser,
+} from 'react-icons/fi';
+import { Button } from '@chakra-ui/button';
+import ClickAwayListener from 'react-click-away-listener';
+
+type UserDropdownProps = {
+  user: any;
+  logout: () => void;
+};
+
+const UserDropdown = ({ user, logout }: UserDropdownProps) => {
+  // dropdown props
+  const [dropdownPopoverShow, setDropdownPopoverShow] = React.useState(false);
+  const btnDropdownRef = React.createRef<any>();
+  const popoverDropdownRef = React.createRef<any>();
+  const openDropdownPopover = () => {
+    setDropdownPopoverShow(true);
+    createPopper(btnDropdownRef.current, popoverDropdownRef.current, {
+      placement: 'bottom-start',
+    });
+  };
+  const closeDropdownPopover = () => {
+    setDropdownPopoverShow(false);
+  };
+  return (
+    <>
+      <Button
+        display='flex'
+        alignItems='center'
+        colorScheme='red'
+        variant='ghost'
+        mr='4'
+        ref={btnDropdownRef}
+        onClick={(e) => {
+          e.preventDefault();
+          dropdownPopoverShow ? closeDropdownPopover() : openDropdownPopover();
+        }}
+      >
+        <FiUserCheck />
+        <Text mx='2'>Hi, {user.firstName}</Text>
+        {dropdownPopoverShow ? <FiChevronUp /> : <FiChevronDown />}
+      </Button>
+      <ClickAwayListener
+        onClickAway={() => dropdownPopoverShow && closeDropdownPopover()}
+      >
+        <Box
+          as={List}
+          ref={popoverDropdownRef}
+          hidden={!dropdownPopoverShow}
+          bg='white'
+          minWidth='48'
+          shadow='lg'
+          rounded='base'
+          zIndex='popover'
+          py={2}
+          listStyleType='none'
+          textAlign='left'
+        >
+          <RouterLink to='/'>
+            <ListItem
+              bg='transparent'
+              _hover={{ bg: 'gray.200' }}
+              py='2'
+              px='4'
+              fontSize='md'
+              color='gray.800'
+              display='flex'
+              alignItems='center'
+              onClick={closeDropdownPopover}
+            >
+              <FiUser />
+              <Text mx='2'>Account</Text>
+            </ListItem>
+          </RouterLink>
+          <RouterLink to='/'>
+            <ListItem
+              bg='transparent'
+              _hover={{ bg: 'gray.200' }}
+              py='2'
+              px='4'
+              fontSize='md'
+              color='gray.800'
+              display='flex'
+              alignItems='center'
+              onClick={closeDropdownPopover}
+            >
+              <FiInbox />
+              <Text mx='2'>Order</Text>
+            </ListItem>
+          </RouterLink>
+          <RouterLink to='/' onClick={() => logout()}>
+            <ListItem
+              bg='transparent'
+              _hover={{ bg: 'gray.200' }}
+              py='2'
+              px='4'
+              fontSize='md'
+              color='gray.800'
+              display='flex'
+              alignItems='center'
+              onClick={closeDropdownPopover}
+            >
+              <FiLogOut />
+              <Text mx='2'>Logout</Text>
+            </ListItem>
+          </RouterLink>
+        </Box>
+      </ClickAwayListener>
+    </>
+  );
+};
+
+export default UserDropdown;
