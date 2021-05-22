8664262f7ea22648a33862143a8f1a32d2c12176
sadewole <samueladewole15@gmail.com>
2021-05-22 16:54:43 +0100

    serach results validation

--- src/views/menu/Catalog/Catalog.tsx
+++ src/views/menu/Catalog/Catalog.tsx
@@ -1,5 +1,4 @@
-import { Image } from '@chakra-ui/image';
-import { Box, Container, Grid, Text } from '@chakra-ui/layout';
+import { Box, Container, Grid } from '@chakra-ui/layout';
 import React from 'react';
 import LargeScreenCard from './Cards/LargeScreenCard';
 import MobileCard from './Cards/MobileCard';
@@ -7,50 +6,25 @@ import { Meal } from './models';
 
 import './catalog.css';
 
-const EmptyCatalog = () => (
-  <Container
-    display='flex'
-    alignItems='center'
-    justifyContent='center'
-    textAlign='center'
-    flexDirection='column'
-    position='relative'
-  >
-    <Image src='./images/fast_food.svg' alt='Fast food' mb='2' width='200px' />
-    <Text fontSize='3xl' fontWeight='bold'>
-      We're sorry!
-    </Text>
-    <Text fontSize={{ base: '2xl', sm: '3xl' }}>
-      We will make all meal available very soon.
-    </Text>
-  </Container>
-);
-
 const Catalog = ({ meals }: { meals: any }) => {
   return (
-    <Box my='10'>
-      {meals && meals.length ? (
-        <Container maxWidth='container.xl'>
-          <Grid
-            templateColumns={{
-              base: '1fr',
-              md: 'repeat(3, 1fr)',
-              lg: 'repeat(4, 1fr)',
-            }}
-            gap={4}
-          >
-            {meals.map((meal: Meal, index: number) => (
-              <Box key={index}>
-                <LargeScreenCard className='smHidden' meal={meal} />
-                <MobileCard className='mdHidden' meal={meal} />
-              </Box>
-            ))}
-          </Grid>
-        </Container>
-      ) : (
-        <EmptyCatalog />
-      )}
-    </Box>
+    <Container maxWidth='container.xl'>
+      <Grid
+        templateColumns={{
+          base: '1fr',
+          md: 'repeat(3, 1fr)',
+          lg: 'repeat(4, 1fr)',
+        }}
+        gap={4}
+      >
+        {meals.map((meal: Meal, index: number) => (
+          <Box key={index}>
+            <LargeScreenCard className='smHidden' meal={meal} />
+            <MobileCard className='mdHidden' meal={meal} />
+          </Box>
+        ))}
+      </Grid>
+    </Container>
   );
 };
 
--- src/views/menu/Menu.tsx
+++ src/views/menu/Menu.tsx
@@ -4,12 +4,48 @@ import Hero from './Hero';
 import { useDispatch, useSelector } from 'src/store';
 import { fetchMeals } from 'src/slices/meal';
 import { Meal } from './Catalog/models';
+import { Image } from '@chakra-ui/image';
+import { Box, Container, Text } from '@chakra-ui/layout';
+
+type EmptyCatalogProps = {
+  headText: string;
+  subText: string;
+};
+
+const noResult = {
+  headText: 'No search results.',
+  subText: 'Kindly clear your search to see our available meal.',
+};
+
+const noMeal = {
+  headText: 'Meal catalog is empty!',
+  subText: 'We will make all meal available very soon.',
+};
+
+const EmptyCatalog = ({ headText, subText }: EmptyCatalogProps) => (
+  <Container
+    display='flex'
+    alignItems='center'
+    justifyContent='center'
+    textAlign='center'
+    flexDirection='column'
+    position='relative'
+  >
+    <Image src='./images/fast_food.svg' alt='Fast food' mb='2' width='200px' />
+    <Text fontSize='3xl' fontWeight='bold'>
+      {headText}
+    </Text>
+    <Text fontSize={{ base: '2xl', sm: '3xl' }}>{subText}</Text>
+  </Container>
+);
 
 const ExploreMeals = () => {
   const [search, setSearch] = useState<string>('');
   const dispatch = useDispatch();
   // @ts-ignore
   const { allMeal } = useSelector((state) => state.meal);
+
+  let emptyCatalog: EmptyCatalogProps = noMeal;
 
   useEffect(() => {
     dispatch(fetchMeals());
@@ -19,6 +55,12 @@ const ExploreMeals = () => {
     meal.name.toLowerCase().includes(search.toLowerCase())
   );
 
+  if (allMeal && allMeal.length) {
+    if (!meals.length) {
+      emptyCatalog = noResult;
+    }
+  }
+
   return (
     <div>
       <Hero
@@ -27,7 +69,13 @@ const ExploreMeals = () => {
         }}
         search={search}
       />
-      <Catalog meals={meals} />
+      <Box my='10'>
+        {meals && meals.length ? (
+          <Catalog meals={meals} />
+        ) : (
+          <EmptyCatalog {...emptyCatalog} />
+        )}
+      </Box>
     </div>
   );
 };
