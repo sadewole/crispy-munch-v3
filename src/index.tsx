d84023bd24c3246253f605e6df0f0ed60413c912
sadewole <samueladewole15@gmail.com>
2021-06-18 23:45:27 +0100

    clean ups

--- src/index.tsx
+++ src/index.tsx
@@ -18,7 +18,7 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.unregister();
+serviceWorker.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
